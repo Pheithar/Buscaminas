a78e077548db9f3a65558e70f894b285db9ffb65
apmpm <alejandrpm@gmail.com>
2019-02-10 20:50:43 +0100

    up

--- pruebas/Prueba carga imagenes.py
+++ pruebas/Prueba carga imagenes.py
@@ -1,14 +1,8 @@
-
-print(open("images.txt"))
-
-texto = open("images.txt").readlines()
+texto = open("../Juego/images.txt", "r").readlines()
 longitud_texto = len(texto)
 
-<<<<<<< HEAD
-=======
 for i in range(0, longitud_texto):
-    elemento = texto[0].split()
+    elemento = texto[i].split()
 
->>>>>>> 4aed31b93c3ac5f2e24484e4bcd3013fcae7ef6d
-    if elemento[0] == "0Cell":
+    if elemento[0] == "1Cell":
         print(elemento[1])
