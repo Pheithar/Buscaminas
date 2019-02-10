cf3f67231cfe8de2f9d6fcc58e90d23066af2883
apmpm <alejandrpm@gmail.com>
2019-02-10 19:26:03 +0100

    nada

--- ejemplos/main.py
+++ ejemplos/main.py
@@ -3,7 +3,9 @@ import escolar.alumnos.Student #importo la clase Student
 from escolar.alumnos import Student() #otra forma de importar la clase.
 
 class Juego():
-    pass
+
+    def __init__(self):
+        self.__x = 10
 
     def main():
         '''aquí es donde se crea la magia'''
@@ -11,6 +13,6 @@ class Juego():
         print (a)
 
     #sirve para que este main solo se pueda ejecutar desde aquí
-        
+
     if __name__ == "__main__"
         main()
