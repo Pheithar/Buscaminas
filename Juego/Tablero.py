ef1da9e3bb640559c5343e83ba16718de4468236
apmpm <alejandrpm@gmail.com>
2019-02-10 20:15:07 +0100

    update

--- Juego/Tablero.py
+++ Juego/Tablero.py
@@ -1,10 +1,12 @@
 import cell.Cell
 
 class Board:
+    #constructor de tablero
     def __init__(self, size_X, size_y, bombs)
     self.size_X  = size_X
     self.size_Y = size_Y
     self.bombs = bombs #se pone un número inicial de bombas al crear el tablero
+
     #creamos el tablero inicial
     for i in range(size_X):
         for j in range(size_Y):
