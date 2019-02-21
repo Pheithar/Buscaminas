93a3e8dbb6989385c5e3ea895bc58b1f3e7116e7
Alejandro Valverde <pheithar@hotmail.com>
2019-02-21 14:31:11 +0100

    ¿SOY YO?

--- pruebas/Prueba carga imagenes.py
+++ pruebas/Prueba carga imagenes.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 #texto = open("ppp.txt")
 texto = open("../Juego/images.txt")
 lineas = texto.readlines()
@@ -9,13 +7,6 @@ print(lineas[0])
 
 for i in range(0, longitud_texto):
     elemento = lineas[i].split()
-=======
-texto = open("../Juego/images.txt").readlines()
-longitud_texto = len(texto)
 
-for i in range(0, longitud_texto):
-    elemento = texto[i].split()
-
->>>>>>> 94e23c591f2b40fb82e8109b0f23ccb37004bbdf
     if elemento[0] == "1Cell":
-        print(elemento[1])
+##HI
